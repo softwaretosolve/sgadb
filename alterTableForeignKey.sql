620d5bd96448dbb9d58e86459c55935417cdf45f
Nelson González <njgp86@gmail.com>
2017-01-25 10:08:12 -0600

    Modificación de llaves foráneas del módulo administrativo. Issue: #26

--- alterTableForeignKey.sql
+++ alterTableForeignKey.sql
@@ -84,151 +84,173 @@ ALTER TABLE InstitucionPermiso
 	ADD FOREIGN KEY (PermisoID)
 	REFERENCES Permiso(ID);
 	
-
-ALTER TABLE Materia
-	ADD FOREIGN KEY (EstadoID)
-	REFERENCES Estado(ID);
-	
-	
-ALTER TABLE InstitucionMateria
-	ADD FOREIGN KEY (InstitucionID)
-	REFERENCES Institucion(ID);
-	
-ALTER TABLE InstitucionMateria
-	ADD FOREIGN KEY (MateriaID)
-	REFERENCES Materia(ID);
-	
-	
+#---------------------------- ORGANIZACIÓN ACADÉMICA ---------------------------------
 ALTER TABLE CategoriaCiclo
 	ADD FOREIGN KEY (CategoriaCicloPadreID)
-	REFERENCES CategoriaCiclo(ID);
-
+	REFERENCES CategoriaCiclo(CategoriaCicloID);
 	
-ALTER TABLE InstitucionCategoriaCiclo
+ALTER TABLE CategoriaCiclo
 	ADD FOREIGN KEY (InstitucionID)
-	REFERENCES Institucion(ID);
+	REFERENCES Institucion(InstitucionID);
 	
-ALTER TABLE InstitucionCategoriaCiclo
-	ADD FOREIGN KEY (CategoriaCicloID)
-	REFERENCES CategoriaCiclo(ID);
+ALTER TABLE CategoriaCiclo
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
 	
-	
+#-----------------------------------------------------	
 ALTER TABLE Ciclo
 	ADD FOREIGN KEY (CategoriaCicloID)
-	REFERENCES CategoriaCiclo(ID);
+	REFERENCES CategoriaCiclo(CategoriaCicloID);
 	
 ALTER TABLE Ciclo
 	ADD FOREIGN KEY (CicloPadreID)
-	REFERENCES Ciclo(ID);
-	
-
-ALTER TABLE InstitucionCiclo
+	REFERENCES Ciclo(CicloID);
+		
+ALTER TABLE Ciclo
 	ADD FOREIGN KEY (InstitucionID)
-	REFERENCES Institucion(ID);
-	
-ALTER TABLE InstitucionCiclo
-	ADD FOREIGN KEY (CicloID)
-	REFERENCES Ciclo(ID);
-	
+	REFERENCES Institucion(InstitucionID);
+		
+ALTER TABLE Ciclo
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
+
+#-----------------------------------------------------
 	
 ALTER TABLE CategoriaNivel
 	ADD FOREIGN KEY (CategoriaNivelPadreID)
-	REFERENCES CategoriaNivel(ID);
-
+	REFERENCES CategoriaNivel(CategoriaNivelID);
 	
-ALTER TABLE InstitucionCategoriaNivel
+ALTER TABLE CategoriaNivel
 	ADD FOREIGN KEY (InstitucionID)
-	REFERENCES Institucion(ID);
+	REFERENCES Institucion(InstitucionID);
 	
-ALTER TABLE InstitucionCategoriaNivel
-	ADD FOREIGN KEY (CategoriaNivelID)
-	REFERENCES CategoriaNivel(ID);
-	
-	
+ALTER TABLE CategoriaNivel
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
+
+#-----------------------------------------------------
+
 ALTER TABLE Nivel
 	ADD FOREIGN KEY (CategoriaNivelID)
-	REFERENCES CategoriaNivel(ID);
+	REFERENCES CategoriaNivel(CategoriaNivelID);
 	
 ALTER TABLE Nivel
 	ADD FOREIGN KEY (NivelPadreID)
-	REFERENCES Nivel(ID);
-	
-	
-ALTER TABLE InstitucionNivel
+	REFERENCES Nivel(NivelID);
+		
+ALTER TABLE Nivel
 	ADD FOREIGN KEY (InstitucionID)
-	REFERENCES Institucion(ID);
+	REFERENCES Institucion(InstitucionID);
 	
-ALTER TABLE InstitucionNivel
-	ADD FOREIGN KEY (NivelID)
-	REFERENCES Nivel(ID);
-	
-	
+ALTER TABLE Nivel
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
+
+#-----------------------------------------------------
+
 ALTER TABLE Seccion	
-	ADD FOREIGN KEY (InstitucionNivelID)
-	REFERENCES InstitucionNivel(ID);
+	ADD FOREIGN KEY (NivelID)
+	REFERENCES Nivel(NivelID);
 	
 ALTER TABLE Seccion
 	ADD FOREIGN KEY (EstadoID)
-	REFERENCES Estado(ID);
-
+	REFERENCES Estado(EstadoID);
+	
+#-----------------------------------------------------
+	
+ALTER TABLE Materia
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
+	
+ALTER TABLE Materia
+	ADD FOREIGN KEY (InstitucionID)
+	REFERENCES Institucion(InstitucionID);
+	
+#-----------------------------------------------------
+	
+ALTER TABLE Curriculo
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
+	
+ALTER TABLE Curriculo
+	ADD FOREIGN KEY (InstitucionID)
+	REFERENCES Institucion(InstitucionID);
+	
+#-----------------------------------------------------
 	
 ALTER TABLE Asignatura
 	ADD FOREIGN KEY (CurriculoID)
-	REFERENCES Curriculo(ID);
+	REFERENCES Curriculo(CurriculoID);
 	
 ALTER TABLE Asignatura
 	ADD FOREIGN KEY (MateriaID)
-	REFERENCES Materia(ID);
+	REFERENCES Materia(MateriaID);	
 	
+#-----------------------------------------------------
 	
-ALTER TABLE InstitucionNivelCurriculo
-	ADD FOREIGN KEY (InstitucionNivelID)
-	REFERENCES InstitucionNivel(ID);
+ALTER TABLE NivelCurriculo
+	ADD FOREIGN KEY (NivelID)
+	REFERENCES Nivel(NivelID);
 	
-ALTER TABLE InstitucionNivelCurriculo
+ALTER TABLE NivelCurriculo
 	ADD FOREIGN KEY (CurriculoID)
-	REFERENCES Curriculo(ID);
+	REFERENCES Curriculo(CurriculoID);
 	
+ALTER TABLE NivelCurriculo
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
+	
+#-----------------------------------------------------
 	
 ALTER TABLE Unidad
 	ADD FOREIGN KEY (AsignaturaID)
-	REFERENCES Asignatura(ID);
+	REFERENCES Asignatura(AsignaturaID);
 
 ALTER TABLE Unidad
 	ADD FOREIGN KEY (CicloID)
-	REFERENCES Ciclo(ID);
-	
+	REFERENCES Ciclo(CicloID);
 
-ALTER TABLE InstitucionTipoActividad
+#-----------------------------------------------------
+	
+ALTER TABLE TipoActividad
 	ADD FOREIGN KEY (InstitucionID)
-	REFERENCES Institucion(ID);
+	REFERENCES Institucion(InstitucionID);
 	
-ALTER TABLE InstitucionTipoActividad
-	ADD FOREIGN KEY (TipoActividadID)
-	REFERENCES TipoActividad(ID);
-	
+ALTER TABLE TipoActividad
+	ADD FOREIGN KEY (EstadoID)
+	REFERENCES Estado(EstadoID);
 
+#-----------------------------------------------------
+	
 ALTER TABLE Actividad
 	ADD FOREIGN KEY (TipoActividadID)
-	REFERENCES TipoActividad(ID);
+	REFERENCES TipoActividad(TipoActividadID);
 
 ALTER TABLE Actividad
 	ADD FOREIGN KEY (UnidadID)
-	REFERENCES Unidad(ID);
+	REFERENCES Unidad(UnidadID);
 	
+ALTER TABLE Actividad
+	ADD FOREIGN KEY (SeccionID)
+	REFERENCES Seccion(SeccionID);
 	
+#-----------------------------------------------------
+
 ALTER TABLE Curso
 	ADD FOREIGN KEY (CicloID)
-	REFERENCES Ciclo(ID);
+	REFERENCES Ciclo(CicloID);
 
 ALTER TABLE Curso
 	ADD FOREIGN KEY (AsignaturaID)
-	REFERENCES Asignatura(ID);
+	REFERENCES Asignatura(AsignaturaID);
 	
 ALTER TABLE Curso
 	ADD FOREIGN KEY (SeccionID)
-	REFERENCES Seccion(ID);
+	REFERENCES Seccion(SeccionID);
+	
+#-----------------------------------------------------
 
+#-----------------------------FIN DE MÓDULO DE ORGANIZACIÓN ACADÉMICA--------------------------------
 	
 	
 ALTER TABLE Matricula
